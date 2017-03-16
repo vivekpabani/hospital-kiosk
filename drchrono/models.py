40acee177ff5cadc36ae0c92661208ace090a144
Vivek Pabani <vpabani@hawk.iit.edu>
2017-03-16 17:56:34 -0500

    Add Appointment model to keep track of timing

--- drchrono/models.py
+++ drchrono/models.py
@@ -8,4 +8,23 @@ class Doctor(models.Model):
     """
 
     user = models.ForeignKey(User)
-    pin = models.CharField(max_length=4, default='', blank=True)
+    pin = models.CharField(max_length=4, null=True, blank=True)
+
+
+class Appointment(models.Model):
+    """
+    Model to represent the appointment instance.
+    Data not stored on drchrono, and required to
+    calculate time related measures are stored here.
+    """
+
+    status_choices = (('Confirmed', 'Confirmed'),
+                      ('Arrived', 'Arrived'),
+                      ('In Session', 'In Session'),
+                      ('Complete', 'Complete'))
+
+    appointment_id = models.CharField(max_length=30)
+    scheduled_time = models.DateField()
+    arrival_time = models.DateField(null=True, blank=True)
+    checkup_time = models.DateField(null=True, blank=True)
+    status = models.CharField(max_length=50, choices=status_choices)
