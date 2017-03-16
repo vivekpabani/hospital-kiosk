58a7bd4fd60f2c3ef502ab1f1b0eb6fd46a94c9e
Vivek Pabani <vpabani@hawk.iit.edu>
2017-03-16 14:14:04 -0500

    Add kiosk and pinset urls

--- drchrono/urls.py
+++ drchrono/urls.py
@@ -7,6 +7,8 @@ import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^set_kiosk_pin/', views.set_kiosk_pin, name='set_kiosk_pin'),
+    url(r'^kiosk/', views.kiosk, name='kiosk'),
     url(r'^login_view/', views.login_view, name='login_view'),
     url(r'^logout_view/', views.logout_view, name='logout_view'),
     url(r'', include('social.apps.django_app.urls', namespace='social')),
