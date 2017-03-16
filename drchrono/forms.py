ac31a7900327c968368a25b08f0b1d7cff682891
Vivek Pabani <vpabani@hawk.iit.edu>
2017-03-16 17:55:47 -0500

    Create checkin form

--- drchrono/forms.py
+++ drchrono/forms.py
@@ -4,3 +4,8 @@ from django import forms
 class KioskPinForm(forms.Form):
 
     pin = forms.CharField(max_length=10, label="Enter Pin")
+
+
+class CheckInForm(forms.Form):
+
+    appointment_id = forms.IntegerField(required=True, widget=forms.HiddenInput())
