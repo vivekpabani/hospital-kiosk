f9c5c785936b66b2f0df089014e2595de07c6be4
Vivek Pabani <vpabani@hawk.iit.edu>
2017-03-17 16:29:01 -0500

    Add docstrings, make initial form data compact

--- drchrono/views.py
+++ drchrono/views.py
@@ -15,7 +15,8 @@ from .forms import (KioskPinForm,
 from .utils import (get_current_user_data,
                     get_doctor_appointments,
                     update_patient_demographics,
-                    update_appointment_status,
+                    update_appointment_on_server,
+                    calculate_average_wait_time,
                     get_patient_details_by_id)
 
 @login_required
@@ -30,14 +31,16 @@ def home(request):
 
     # if doctor's first time on kiosk, ask to set up pin. 
     doctor = Doctor.objects.filter(user=request.user)
+
     if not doctor or doctor[0].pin == Doctor._meta.get_field('pin').get_default():
         return redirect('set_kiosk_pin/')
 
     request.session['doctor_validated'] = False 
     request.session['kiosk_pin_set'] = True
 
-    return redirect('kiosk/')
+    return redirect('/kiosk/')
 
+@login_required
 def set_kiosk_pin(request):
     """
     process kiosk pin form, and set up if valid pin.
@@ -45,8 +48,11 @@ def set_kiosk_pin(request):
     """    
 
     if request.method == 'POST':
+
         kiosk_pin_form = KioskPinForm(request.POST)
+
         if kiosk_pin_form.is_valid():
+
             data = kiosk_pin_form.cleaned_data
             pin = data['pin']
 
@@ -57,9 +63,9 @@ def set_kiosk_pin(request):
 
     else:
         kiosk_pin_form = KioskPinForm()
-        return render(request, 'kiosk_pin.html', {'kiosk_pin_form':kiosk_pin_form})
+        return render(request, 'kiosk_pin.html', {'kiosk_pin_form': kiosk_pin_form})
 
-
+@login_required
 def kiosk(request):
     """
     Display today's appointments with option to check in.
@@ -97,13 +103,15 @@ def kiosk(request):
             appointment_instance.save()
 
         # If status is Confirmed, add to list to display to user.
-        if appointment_instance.status in ("", "Confirmed"):
+        if appointment_instance.status in ("", "Confirmed", "Arrived"):
+
             appointment = dict() 
             appointment['id'] = appointment_instance.appointment_id
             appointment['scheduled_time'] = appointment_instance.scheduled_time
+            appointment['status'] = appointment_instance.status
 
-            checkin_form = CheckInForm(initial={'appointment_id':appointment_instance.appointment_id,
-                                            'patient_id':appointment_instance.patient_id})
+            checkin_form = CheckInForm(initial={'appointment_id': appointment_instance.appointment_id,
+                                                'patient_id': appointment_instance.patient_id})
                                    
             appointment['checkin_form'] = checkin_form
 
@@ -113,6 +121,7 @@ def kiosk(request):
 
     return render(request, 'kiosk.html', context)
 
+@login_required
 def validate_patient(request, appointment_id=None):
     """
     Process validation form, and validate user.
@@ -139,6 +148,7 @@ def validate_patient(request, appointment_id=None):
 
             patient_details = get_patient_details_by_id(access_token, patient_id)
 
+            # if validated, go to update demographics 
             if patient_details['first_name'] == first_name and patient_details['last_name'] == last_name:
 
                 request.session['validated_patient_appointment'] = str(appointment_id)
@@ -146,23 +156,29 @@ def validate_patient(request, appointment_id=None):
 
                 return redirect(update_demographics_redirect)
             else:
+                messages.warning(request, 'Validation failed. Reconfirm appointment id and your details and try again.')
 
                 return redirect('/')
 
     elif appointment_id:
+        # if called with an appointent id, verify if valid appointment id.
 
         appointments = Appointment.objects.filter(appointment_id=appointment_id)
 
         if not appointments:
 
+            messages.warning(request, 'Invalid appointment ID used for patient validation.')
             return redirect('/')
 
-        patient_validation_form = PatientValidationForm(initial={'appointment_id':appointment_id})
+        patient_validation_form = PatientValidationForm(initial={'appointment_id': appointment_id})
 
-        context = {'patient_validation_form':patient_validation_form}
+        context = {'patient_validation_form': patient_validation_form}
 
         return render(request, 'patient_validation.html', context)
+    else:
 
+        messages.warning(request, 'Appointment ID is required to access patient validation.')
+        return redirect('/')
 
 def update_demographics(request, appointment_id=None):
     """
@@ -180,55 +196,51 @@ def update_demographics(request, appointment_id=None):
 
             data = demographics_form.cleaned_data
 
-            request.session['validated_patient_appointment'] = ''
-
             demographics_updated = update_patient_demographics(access_token, data['patient_id'], data['doctor'], data)
 
-            if demographics_updated or True:
+            if demographics_updated:
 
-                appointment_updated = update_appointment_status(access_token, appointment_id, "Arrived")
+                appointment_updated = update_appointment_on_server(access_token, appointment_id, "Arrived")
 
                 appointment_instance = Appointment.objects.get(appointment_id=data['appointment_id'])
                 appointment_instance.status = "Arrived"
                 appointment_instance.arrival_time = datetime.datetime.now()
                 appointment_instance.save()
+                messages.success(request, 'Checked in successfully for appointment: ' + str(appointment_instance.appointment_id))
 
             return redirect('/')
 
     elif appointment_id:
+        # if called with an appointent id, verify if valid appointment id, and if patient is validated
+        # before taking to demographic form page. 
 
         appointments = Appointment.objects.filter(appointment_id=appointment_id)
 
         if not appointments:
+
+            messages.warning(request, 'Invalid appointment ID used for updating patient demographics.')
             return redirect('/')
 
         if request.session.get('validated_patient_appointment', '') != str(appointment_id):
+
+            messages.warning(request, 'You need to validate first before updating patient demographics.')
             return redirect('/')
 
         patient_id = appointments[0].patient_id
         
         patient_details = get_patient_details_by_id(access_token, patient_id)
-
-        form_initial_values = {'first_name': patient_details['first_name'],
-                               'last_name': patient_details['last_name'],
-                               'gender': patient_details['gender'],
-                               'appointment_id': appointment_id,
-                               'patient_id': patient_details['id'],
-                               'doctor': patient_details['doctor'],
-                               'address': patient_details['address'],
-                               'home_phone': patient_details['home_phone'],
-                               'cell_phone': patient_details['cell_phone'],
-                               'email': patient_details['email']
-                               }
+        form_initial_values = patient_details
+        form_initial_values['appointment_id'] = appointment_id
+        form_initial_values['patient_id'] = patient_id 
 
         demographics_form = DemographicsForm(initial = form_initial_values)
         context = {'demographics_form': demographics_form}
 
         return render(request, 'update_demographics.html', context)
-
     else:
         return redirect('/')
 
+@login_required
 def doctor_kiosk(request):
     """
     Doctor's view of kiosk which gives him option to update status  
@@ -252,7 +264,10 @@ def doctor_kiosk(request):
         appointment['appointment_status_form'] = appointment_status_form
         appointments.append(appointment)
 
-    context = {'appointments': appointments}
+    average_wait_time = calculate_average_wait_time(request.user)
+
+    context = {'appointments': appointments,
+               'average_wait_time': average_wait_time }
 
     return render(request, 'doctor_kiosk.html', context)
 
@@ -260,6 +275,8 @@ def update_appointment_status(request):
     """
     Update appointment status, and change timing accordingly.
     """
+
+    access_token = request.user.social_auth.get(provider='drchrono').extra_data['access_token']
 
     if request.method == 'POST':
 
@@ -274,12 +291,16 @@ def update_appointment_status(request):
             try:
                 appointment_instance = Appointment.objects.get(appointment_id=appointment_id)
             except Appointment.DoesNotExist:
+                messages.warning(request, 'No records found for given appointment id. Try again.')
                 return redirect('/doctor_kiosk/') 
 
+            # update appointment status on server and database both.
             appointment_instance.update_status(status)
+            update_appointment_on_server(access_token, appointment_id, status)
 
     return redirect('/doctor_kiosk/')    
 
+@login_required
 def validate_doctor(request):
     """
     Validate doctor with pin when he tries to access doctor kiosk.
@@ -303,6 +324,7 @@ def validate_doctor(request):
                 request.session['doctor_validated'] = True 
                 return redirect('/doctor_kiosk/')
             else:
+                messages.warning(request, 'Cannot validate with entered pin. Recheck the pin and try again.')
                 return redirect('/')
     else:
         doctor_validation_form = DoctorValidationForm()
