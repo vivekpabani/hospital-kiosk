e561c13dd64a931f4680f40c60b41382d4d991ab
Vivek Pabani <vpabani@hawk.iit.edu>
2017-03-16 20:25:20 -0500

    Add validate patient urls

--- drchrono/urls.py
+++ drchrono/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^set_kiosk_pin/', views.set_kiosk_pin, name='set_kiosk_pin'),
     url(r'^kiosk/', views.kiosk, name='kiosk'),
+    url(r'^validate_patient/$', views.validate_patient, name='validate_patient'),
+    url(r'^validate_patient/(?P<appointment_id>[0-9]+)/', views.validate_patient, name='validate_patient'),
     url(r'^login_view/', views.login_view, name='login_view'),
     url(r'^logout_view/', views.logout_view, name='logout_view'),
     url(r'', include('social.apps.django_app.urls', namespace='social')),
