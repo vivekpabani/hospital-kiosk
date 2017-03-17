b54bcd025859d7d9fc27ee6abd22728e197c5e17
Vivek Pabani <vpabani@hawk.iit.edu>
2017-03-16 20:22:30 -0500

    Add form for patient validation

--- drchrono/forms.py
+++ drchrono/forms.py
@@ -10,3 +10,9 @@ class CheckInForm(forms.Form):
 
     appointment_id = forms.CharField(required=True, widget=forms.HiddenInput())
     patient_id = forms.CharField(required=True, widget=forms.HiddenInput())
+
+class PatientValidationForm(forms.Form):
+
+    first_name = forms.CharField(required=True, max_length=50, label="First Name")
+    last_name = forms.CharField(required=True, max_length=50, label="Last Name")
+    patient_id = forms.CharField(required=True, widget=forms.HiddenInput())
