0d2b766d6924e16452d31214b596dd9761be5cf4
Vivek Pabani <vpabani@hawk.iit.edu>
2017-03-17 11:51:41 -0500

    Create views for appointment and demographics update

--- drchrono/views.py
+++ drchrono/views.py
@@ -2,14 +2,18 @@ from django.contrib.auth import logout as auth_logout
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from datetime import datetime
+import datetime
 
 from .models import Doctor, Appointment
 from .forms import (KioskPinForm,
                     PatientValidationForm,
+                    DemographicsForm,
+                    AppointmentStatusForm,
                     CheckInForm) 
 from .utils import (get_current_user_data,
                     get_doctor_appointments,
+                    update_patient_demographics,
+                    update_appointment_status,
                     get_patient_details_by_id)
 
 @login_required
@@ -73,31 +77,31 @@ def kiosk(request):
     datetime_format = '%Y-%m-%dT%H:%M:%S'
 
     for data in appointments_data:
-        appointment_id = data['id']
-        scheduled_time = datetime.strptime(data['scheduled_time'], datetime_format)
-        status = data['status']
-        patient_id = data['patient']
+
+        scheduled_time = datetime.datetime.strptime(data['scheduled_time'], datetime_format)
 
         # Add appointment to database, if doesn't exist.
-        if not Appointment.objects.filter(appointment_id=data['id']):
-            appointment_instance = Appointment(appointment_id=appointment_id,
-                                               patient_id=patient_id,
+        try:
+            appointment_instance = Appointment.objects.get(appointment_id=data['id'])
+        except Appointment.DoesNotExist:
+            appointment_instance = Appointment(appointment_id=data['id'],
+                                               patient_id=data['patient'],
                                                scheduled_time=scheduled_time,
-                                               status=status)
-
+                                               status=data['status'])
             appointment_instance.save()
 
-        # Add to list to display to user.
-        appointment = dict() 
-        appointment['id'] = appointment_id
-        appointment['scheduled_time'] = scheduled_time
+        # If status is Confirmed, add to list to display to user.
+        if appointment_instance.status in ("", "Confirmed"):
+            appointment = dict() 
+            appointment['id'] = appointment_instance.appointment_id
+            appointment['scheduled_time'] = appointment_instance.scheduled_time
 
-        checkin_form = CheckInForm(initial={'appointment_id':appointment_id,
-                                            'patient_id':patient_id})
+            checkin_form = CheckInForm(initial={'appointment_id':appointment_instance.appointment_id,
+                                            'patient_id':appointment_instance.patient_id})
                                    
-        appointment['checkin_form'] = checkin_form
+            appointment['checkin_form'] = checkin_form
 
-        appointments.append(appointment)
+            appointments.append(appointment)
 
     context['appointments'] = appointments
 
@@ -123,13 +127,60 @@ def validate_patient(request, appointment_id=None):
 
             first_name = data['first_name']
             last_name = data['last_name']
-            patient_id = data['patient_id']
+            appointment_id = data['appointment_id']
+            appointments = Appointment.objects.filter(appointment_id=appointment_id)
+            patient_id = appointments[0].patient_id
 
             patient_details = get_patient_details_by_id(access_token, patient_id)
 
             if patient_details['first_name'] == first_name and patient_details['last_name'] == last_name:
-                update_demographic_rediret = '/update_demographics/' + appointment_id + '/' + patient_id + '/'
+                request.session['validated_patient_appointment'] = str(appointment_id)
+                update_demographics_redirect = '/update_demographics/' + str(appointment_id) + '/'
                 return redirect(update_demographics_redirect)
+
+            return redirect('/')
+
+    elif appointment_id:
+
+        appointments = Appointment.objects.filter(appointment_id=appointment_id)
+
+        if not appointments:
+
+            return redirect('/')
+
+        patient_validation_form = PatientValidationForm(initial={'appointment_id':appointment_id})
+
+        return render(request, 'patient_validation.html', {'patient_validation_form':patient_validation_form})
+
+
+def update_demographics(request, appointment_id=None):
+    """
+    Update demographics of a patient with the form data in case of POST request.  
+    Otherwise populate the form with patient's data and allow to update.
+    """
+
+    access_token = request.user.social_auth.get(provider='drchrono').extra_data['access_token']
+
+    if request.method == 'POST':
+
+        demographics_form = DemographicsForm(request.POST)
+
+        if demographics_form.is_valid():
+
+            data = demographics_form.cleaned_data
+
+            request.session['validated_patient_appointment'] = ''
+
+            demographics_updated = update_patient_demographics(access_token, data['patient_id'], data['doctor'], data)
+
+            if demographics_updated or True:
+
+                appointment_updated = update_appointment_status(access_token, appointment_id, "Arrived")
+
+                appointment_instance = Appointment.objects.get(appointment_id=data['appointment_id'])
+                appointment_instance.status = "Arrived"
+                appointment_instance.arrival_time = datetime.datetime.now()
+                appointment_instance.save()
 
             return redirect('/')
 
@@ -140,11 +191,80 @@ def validate_patient(request, appointment_id=None):
         if not appointments:
             return redirect('/')
 
-        patient_id = appointments[0].patient_id 
+        if request.session.get('validated_patient_appointment', '') != str(appointment_id):
+            return redirect('/')
 
-        patient_validation_form = PatientValidationForm(initial={'patient_id':patient_id})
+        patient_id = appointments[0].patient_id
+        
+        patient_details = get_patient_details_by_id(access_token, patient_id)
 
-        return render(request, 'patient_validation.html', {'patient_validation_form':patient_validation_form})
+        form_initial_values = {'first_name': patient_details['first_name'],
+                               'last_name': patient_details['last_name'],
+                               'gender': patient_details['gender'],
+                               'appointment_id': appointment_id,
+                               'patient_id': patient_details['id'],
+                               'doctor': patient_details['doctor'],
+                               'address': patient_details['address'],
+                               'home_phone': patient_details['home_phone'],
+                               'cell_phone': patient_details['cell_phone'],
+                               'email': patient_details['email']
+                               }
+
+        demographics_form = DemographicsForm(initial = form_initial_values)
+        return render(request, 'update_demographics.html', {'demographics_form':demographics_form})
+
+    else:
+        return redirect('/')
+
+def doctor_kiosk(request):
+    """
+    Doctor's view of kiosk which gives him option to update status  
+    and check the wait time of patients.
+    """
+
+    appointment_instances = Appointment.objects.filter(scheduled_time__contains=datetime.date.today())
+    appointments = list()
+
+    for appointment_instance in appointment_instances:
+
+        appointment = {'scheduled_time': appointment_instance.scheduled_time,
+                       'arrival_time': appointment_instance.arrival_time,
+                       'status': appointment_instance.status,
+                       'appointment_id': appointment_instance.appointment_id,
+                       'patient_id': appointment_instance.patient_id}
+
+        appointment_status_form = AppointmentStatusForm(initial={'status': appointment_instance.status,
+                                                                 'appointment_id': appointment_instance.appointment_id})
+
+        appointment['appointment_status_form'] = appointment_status_form
+        appointments.append(appointment)
+
+    context = {'appointments': appointments}
+
+    return render(request, 'doctor_kiosk.html', context)
+
+def update_appointment_status(request):
+    """
+    Update appointment status, and change timing accordingly.
+    """
+    print("update_appointment_status")
+    if request.method == 'POST':
+        appointment_status_form = AppointmentStatusForm(request.POST)
+        if appointment_status_form.is_valid():
+            print("valid form")
+            data = appointment_status_form.cleaned_data
+            appointment_id = data['appointment_id']
+            status = data['status']
+            print("appointment_id", appointment_id)
+            try:
+                appointment_instance = Appointment.objects.get(appointment_id=appointment_id)
+            except Appointment.DoesNotExist:
+                print("In Except") 
+                return redirect('/doctor_kiosk/') 
+
+            appointment_instance.update_status(status)
+
+    return redirect('/doctor_kiosk/')    
 
 def login_view(request):
     """
