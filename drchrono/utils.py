4eaad4c6d92634b0efb1c694a3be9e5f5dd70941
Vivek Pabani <vpabani@hawk.iit.edu>
2017-03-17 16:27:57 -0500

    Implement utility for average time calc

--- drchrono/utils.py
+++ drchrono/utils.py
@@ -1,8 +1,13 @@
 import requests
 import datetime
+from pytz import timezone
 
+from .models import Appointment
 
 def get_current_user_data(access_token):
+    """
+    Fetch current user data from server  
+    """
 
     current_user_url = 'https://drchrono.com/api/users/current'
 
@@ -11,6 +16,9 @@ def get_current_user_data(access_token):
     return response.json()
 
 def get_doctor_appointments(access_token, doctor_id, adate=None):
+    """
+    Get doctor's today's appointments using doctor id. 
+    """
 
     if adate and type(adate) == datetime.date:
         adate = adate.strftime('%Y-%m-%d')
@@ -21,17 +29,23 @@ def get_doctor_appointments(access_token, doctor_id, adate=None):
     appointment_url = appointment_url + '?doctor=' + str(doctor_id) + '&date=' + str(adate)
 
     appointments = list()
+
     while appointment_url:
+
         response = requests.get(appointment_url, headers=get_headers(access_token))
         json = response.json()
+
         for appointment in json['results']:
-            print("appointment", appointment)
             appointments.append(appointment)
+
         appointment_url = json['next']
 
     return appointments
 
 def get_patient_details_by_id(access_token, patient_id):
+    """
+    Get patient's details from server.   
+    """
 
     patients_url = 'https://drchrono.com/api/patients'
     patients_url = patients_url + '/' + str(patient_id) 
@@ -42,31 +56,56 @@ def get_patient_details_by_id(access_token, patient_id):
     return result
 
 def update_patient_demographics(access_token, patient_id, doctor_id, data):
+    """
+    Modify patient demographics on server.
+    """
 
     patients_url = 'https://drchrono.com/api/patients'
     patients_url = patients_url + '/' + str(patient_id)
 
-    data['doctor'] = '/api/doctors/' + str(doctor_id)
+    data['doctor'] = int(doctor_id)
 
     response = requests.put(patients_url, data=data, headers=get_headers(access_token))
 
     return response.status_code in (200, 204)
 
-def update_appointment_status(access_token, appointment_id, new_status):
+def update_appointment_on_server(access_token, appointment_id, new_status):
+    """
+    Modify appointment on server.
+    """
 
     appointments_url = 'https://drchrono.com/api/appointments'
-    appointment_url = appointments_url + '/' + str(appointment_id) 
+    appointments_url = appointments_url + '/' + str(appointment_id) 
 
-    response = requests.get(appointment_url, headers=get_headers(access_token))
-    appointment_data = response.json() 
-
+    appointment_data = requests.get(appointments_url, headers=get_headers(access_token)).json()
     appointment_data['status'] = new_status
 
-    response = requests.put(appointments_url, data=appointment_data, headers=get_headers(access_token))
-    print('response', response)
+    response = requests.patch(appointments_url, data=appointment_data, headers=get_headers(access_token))
 
     return response.status_code in (200, 204)
 
+def calculate_average_wait_time(doctor):
+    """
+    Calculate average wait time of all appointments of given doctor.
+    """
+
+    total_wait_time = 0
+    appointments = Appointment.objects.filter(status__in=['Arrived','In Session','Complete'])
+    tz = timezone('America/Los_Angeles')
+
+    for appointment in appointments:
+
+        if appointment.status == 'Arrived':
+            total_wait_time += (datetime.datetime.now(tz) - appointment.arrival_time).total_seconds() / 60.0 
+        elif appointment.status in ('Complete', 'In Session'):
+            total_wait_time += (appointment.checkup_time - appointment.arrival_time).total_seconds() / 60.0
+
+    return int(total_wait_time/len(appointments))
+
+
 def get_headers(access_token):
+    """
+    Get headers with accesss token
+    """
 
     return {'Authorization': 'Bearer {0}'.format(access_token)}
